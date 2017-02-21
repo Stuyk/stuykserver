5f22ee213f51de7451835d78ffcd2b0f7fe1a304
Trevor Wessel <stuykgaming@gmail.com>
2017-02-20 17:16:53 -0800

    Fishing Polished

--- Jobs/Fishing.cs
+++ Jobs/Fishing.cs
@@ -15,14 +15,37 @@ namespace stuykserver.Jobs
         SpawnPoints spawnPoints = new SpawnPoints();
         DatabaseHandler db = new DatabaseHandler();
         List<Vector3> fishingPoints = new List<Vector3>();
+        List<Vector3> fishingSalePoints = new List<Vector3>();
         Dictionary<Client,string> playersFishing = new Dictionary<Client, string>();
         Dictionary<Client, NetHandle> playersFishingRods = new Dictionary<Client, NetHandle>();
+        Dictionary<Client, NetHandle> playerFishItems = new Dictionary<Client, NetHandle>();
 
         public Fishing()
         {
             API.onResourceStart += API_onResourceStart;
             API.onChatMessage += API_onChatMessage;
             API.onClientEventTrigger += API_onClientEventTrigger;
+            API.onPlayerDisconnected += API_onPlayerDisconnected;
+        }
+
+        private void API_onPlayerDisconnected(Client player, string reason)
+        {
+            if (playersFishing.ContainsKey(player))
+            {
+                playersFishing.Remove(player);
+            }
+
+            if (playersFishingRods.ContainsKey(player))
+            {
+                API.deleteEntity(playersFishingRods[player]);
+                playersFishingRods.Remove(player);
+            }
+
+            if (playerFishItems.ContainsKey(player))
+            {
+                API.deleteEntity(playerFishItems[player]);
+                playerFishItems.Remove(player);
+            }
         }
 
         private void API_onClientEventTrigger(Client player, string eventName, params object[] arguments)
@@ -30,6 +53,7 @@ namespace stuykserver.Jobs
             if(eventName == "fishingFailed")
             {
                 playerFishingFailed(player);
+                putBucketInLeftHand(player);
             }
         }
 
@@ -37,7 +61,7 @@ namespace stuykserver.Jobs
         {
             if (playersFishing.ContainsKey(player))
             {
-                if (message == playersFishing[player])
+                if (message.ToLower() == playersFishing[player].ToLower())
                 {
                     API.sendNotificationToPlayer(player, "Correct!");
                     playersFishing.Remove(player);
@@ -47,6 +71,8 @@ namespace stuykserver.Jobs
                     playersFishingRods.Remove(player);
                     API.stopPlayerAnimation(player);
                     API.stopPedAnimation(player);
+                    playerAddFish(player);
+                    putBucketInLeftHand(player);
                     e.Cancel = true;
                     return;
                 }
@@ -82,6 +108,29 @@ namespace stuykserver.Jobs
             }
 
             API.consoleOutput("Fishing Points Intialized: " + initializedObjects.ToString());
+
+            query = "SELECT ID FROM FishingSalePoints";
+            result = API.exported.database.executeQueryWithResult(query);
+
+            initializedObjects = 0;
+
+            foreach (DataRow row in result.Rows)
+            {
+                foreach (DataColumn column in result.Columns)
+                {
+                    string selectedrow = row[column].ToString();
+
+                    float posX = Convert.ToSingle(db.pullDatabase("FishingSalePoints", "PosX", "ID", selectedrow));
+                    float posY = Convert.ToSingle(db.pullDatabase("FishingSalePoints", "PosY", "ID", selectedrow));
+                    float posZ = Convert.ToSingle(db.pullDatabase("FishingSalePoints", "PosZ", "ID", selectedrow));
+
+                    positionSaleBlips(new Vector3(posX, posY, posZ), new Vector3());
+
+                    initializedObjects = ++initializedObjects;
+                }
+            }
+
+            API.consoleOutput("Fishing Sale Points Intialized: " + initializedObjects.ToString());
         }
 
         [Command("createfishingpoint")]
@@ -100,23 +149,75 @@ namespace stuykserver.Jobs
             }
         }
 
+        [Command("createfishingsalepoint")]
+        public void cmdCreateFishingSellPoint(Client player)
+        {
+            if (db.isAdmin(player.name))
+            {
+                db.insertDataPointPosition("FishingSalePoints", player);
+                positionSaleBlips(new Vector3(player.position.X, player.position.Y, player.position.Z), new Vector3());
+                API.sendNotificationToPlayer(player, "~g~A fishing sale point has been created.");
+            }
+        }
+
+        [Command("sellfish")]
+        public void cmdSellFish(Client player)
+        {
+            if (db.isPlayerLoggedIn(player))
+            {
+                foreach (Vector3 point in fishingSalePoints)
+                {
+                    if (player.position.DistanceTo(point) <= 5)
+                    {
+                        int fish = Convert.ToInt32(db.pullDatabase("PlayerInventory", "Fish", "Nametag", player.name));
+
+                        if (fish <= 0)
+                        {
+                            API.sendNotificationToPlayer(player, "~r~You don't seem to have any fish.");
+                            return;
+                        }
+                        else
+                        {
+                            db.setPlayerMoney(player, 7 * fish);
+                            db.updateDatabase("PlayerInventory", "Fish", 0.ToString(), "Nametag", player.name);
+                            int quantity = Convert.ToInt32(db.pullDatabase("FishingSalePoints", "Quantity", "PosX", point.X.ToString()));
+                            quantity += fish;
+                            db.updateDatabase("FishingSalePoints", "Quantity", quantity.ToString(), "PosX", point.X.ToString());
+                            API.sendNotificationToPlayer(player, string.Format("~b~You sold ~g~{0} ~b~fish.", fish));
+
+                            if (playerFishItems.ContainsKey(player))
+                            {
+                                API.deleteEntity(playerFishItems[player]);
+                                playerFishItems.Remove(player);
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+
         [Command("fish")]
         public void cmdFish(Client player)
         {
             if (!playersFishing.ContainsKey(player))
             {
-                foreach (Vector3 point in fishingPoints)
+                if (Convert.ToInt32(db.pullDatabase("PlayerInventory", "Fish", "Nametag", player.name)) < 10)
                 {
-                    if (player.position.DistanceTo(point) <= 20)
+                    foreach (Vector3 point in fishingPoints)
                     {
-                        string word = sendWordToPlayer();
-                        API.triggerClientEvent(player, "startFishing", word);
-                        playersFishing.Add(player, word);
-                        var fishingrod = API.createObject(-1910604593, new Vector3(), new Vector3());
-                        API.attachEntityToEntity(fishingrod, player, "SKEL_L_Hand", new Vector3(0.13f, 0.1f, 0.01f), new Vector3(180f, 90f, 70f));
-                        playersFishingRods.Add(player, fishingrod);
-                        API.playPlayerAnimation(player, 1, "amb@world_human_stand_fishing@idle_a", "idle_c");
-                        return;
+                        if (player.position.DistanceTo(point) <= 20)
+                        {
+                            string word = sendWordToPlayer();
+                            API.triggerClientEvent(player, "startFishing", word);
+                            playersFishing.Add(player, word);
+                            var fishingrod = API.createObject(-1910604593, new Vector3(), new Vector3());
+                            API.attachEntityToEntity(fishingrod, player, "SKEL_L_Hand", new Vector3(0.13f, 0.1f, 0.01f), new Vector3(180f, 90f, 70f));
+                            playersFishingRods.Add(player, fishingrod);
+                            API.playPlayerAnimation(player, 1, "amb@world_human_stand_fishing@idle_a", "idle_c");
+                            putBucketOnGround(player);
+                            return;
+                        }
                     }
                 }
             }
@@ -126,11 +227,17 @@ namespace stuykserver.Jobs
             }
         }
 
-        public void beginFishing(Client player)
+        public void positionSaleBlips(Vector3 position, Vector3 rotation)
         {
-            
-            return;
+            API.createTextLabel("~y~Usage: ~w~/sellfish", new Vector3(position.X, position.Y, position.Z), 20, 0.5f);
+            var newBlip = API.createBlip(new Vector3(position.X, position.Y, position.Z));
+            API.setBlipSprite(newBlip, 88);
+            API.setBlipColor(newBlip, 63);
+            fishingSalePoints.Add(new Vector3(position.X, position.Y, position.Z));
+            int i = 0;
+            ++i;
         }
+
 
         public void positionBlips(Vector3 position, Vector3 rotation)
         {
@@ -159,6 +266,50 @@ namespace stuykserver.Jobs
             API.playSoundFrontEnd(player, "Hack_Failed", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS");
             API.deleteEntity(playersFishingRods[player]);
             playersFishingRods.Remove(player);
+            API.stopPlayerAnimation(player);
+            API.stopPedAnimation(player);
+            putBucketInLeftHand(player);
+        }
+
+        public void playerAddFish(Client player)
+        {
+            int oldFish = Convert.ToInt32(db.pullDatabase("PlayerInventory", "Fish", "Nametag", player.name));
+            oldFish += 1;
+            db.updateDatabase("PlayerInventory", "Fish", oldFish.ToString(), "Nametag", player.name);
+            API.sendNotificationToPlayer(player, string.Format("Your bucket contains {0}/10 Fish.", oldFish));
+        }
+
+        public void putBucketInLeftHand(Client player)
+        {
+            if (!playerFishItems.ContainsKey(player))
+            {
+                var bucket = API.createObject(4591557, new Vector3(), new Vector3());
+                API.attachEntityToEntity(bucket, player, "SKEL_L_HAND", new Vector3(0.53f, -0.2f, -0.02f), new Vector3(0, 270f, 0));
+                playerFishItems.Add(player, bucket);
+                return;
+            }
+            else
+            {
+                API.attachEntityToEntity(playerFishItems[player], player, "SKEL_L_HAND", new Vector3(0.53f, -0.2f, -0.02f), new Vector3(0, 270f, 0));
+                return;
+            }
+        }
+
+        public void putBucketOnGround(Client player)
+        {
+            if (!playerFishItems.ContainsKey(player))
+            {
+                var bucket = API.createObject(4591557, new Vector3(player.position.X, player.position.Y - 0.5f, player.position.Z - 1.0f), new Vector3());
+                playerFishItems.Add(player, bucket);
+                return;
+            }
+            else
+            {
+                playerFishItems.Remove(player);
+                var bucket = API.createObject(4591557, new Vector3(player.position.X, player.position.Y - 0.5f, player.position.Z - 1.0f), new Vector3());
+                playerFishItems.Add(player, bucket);
+                return;
+            }
         }
     }
 }
--- clientside/resources/Fishing.js
+++ clientside/resources/Fishing.js
@@ -1,11 +1,16 @@
 var storedword = null;
 var currentword = null;
+var queuetask = null;
 var casting = null;
 var resX = API.getScreenResolutionMantainRatio().Width;
 var resY = API.getScreenResolutionMantainRatio().Height;
+var resModX = API.getScreenResolution().Height;
+var resModY = API.getScreenResolution().Width;
 var startTime = 0;
 var castTime = 0;
 var biteTime = 0;
+var secondsLeft = 10;
+var hz = 0;
 
 API.onServerEventTrigger.connect(function (eventName, args) {
 	if (eventName == "startFishing") {
@@ -14,8 +19,12 @@ API.onServerEventTrigger.connect(function (eventName, args) {
 			API.sendNotification("~g~You cast your line.");
 			API.sendNotification("~y~Type the word that appears.");
 			API.playPlayerAnimation("amb@world_human_stand_fishing@idle_a", "idle_c", 1, -1);
-			biteTime = Math.floor((Math.random() * 20) * 100);
+			biteTime = Math.floor((Math.random() * 10 * 350) + 1);
+			if (biteTime < 500) {
+				biteTime += 500;
+			}
 			startTime = 0;
+			secondsLeft = 0;
 			storedword = args[0];
 			casting = true;
 		}
@@ -25,6 +34,7 @@ API.onServerEventTrigger.connect(function (eventName, args) {
 		casting = null;
 		storedword = null;
 		currentword = null;
+		secondsleft = 0;
 		startTime = 0;
 		castTime = 0;
 		biteTime = 0;
@@ -32,35 +42,60 @@ API.onServerEventTrigger.connect(function (eventName, args) {
 	}
 });
 
-API.onUpdate.connect(function() {
+API.onKeyDown.connect(function(player, e) {
+	if (casting != null) {
+		if (!API.isChatOpen() && e.KeyCode == Keys.Space && queuetask != null) {
+			currentword = storedword;
+		}
+		else if (!API.isChatOpen() && e.KeyCode == Keys.Space) {
+			fishingTimeout();
+		}
+	}
+	
+
+});
+
+API.onUpdate.connect(function() { //700 HZ is about 10 Seconds, 350HZ is about 5 seconds, 70HZ is about 1 Second.
 	if (casting != null) {
 		castTime += 1;
+		if (currentword == null) {
+			if (castTime > biteTime) {
+				API.dxDrawTexture("clientside/resources/images/fishing.png", new Point(resX / 2 - 200, resY / 2 - 125), new Size(200, 125), 1);
+				API.drawText("Press Spacebar to Reel In", resX / 2 + 200, resY - 300, 1, 255, 255, 255, 255, 4, 2, true, true, 0);
+				queuetask = true;
+			}
+		}
 		
-		if (castTime > biteTime) {
-			currentword = storedword;
+		if (biteTime > 5000) {
+			fishingTimeout();
 		}
 		
 		if (currentword != null) {
-			API.drawText(currentword, resX / 2 + 75, 300, 1, 255, 0, 0, 255, 4, 2, true, true, 0);
+			API.drawText(currentword, resX / 2 + 100, resY - 300, 1, 187, 255, 175, 255, 4, 2, true, true, 0);
 		
 			if (startTime > 10 && startTime < 13) {
 				API.playSoundFrontEnd("CHECKPOINT_NORMAL", "HUD_MINI_GAME_SOUNDSET");
 			}
 		
-			if (startTime > 500) {
+			if (startTime > 700) {
 				fishingTimeout();
 			}
+			
+			API.drawText(Math.floor(startTime / 70).toString() + "/10", resX / 2 + 200, resY - 200, 1, 255, 255, 255, 255, 4, 2, true, true, 0);
+
 			startTime += 1;
 		}
 	}
-    
 });
 
 function fishingTimeout() {
 	casting = null;
 	storedword = null;
 	currentword = null;
+	queuetask = null;
+	secondsLeft = 0;
 	startTime = 0;
+	API.stopPlayerAnimation();
 	API.sendNotification("~r~You ran out of time.");
 	API.playSoundFrontEnd("Hack_Failed", "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS");
 	API.triggerServerEvent("fishingFailed");
